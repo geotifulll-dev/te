d01366c87544c42a417e97eebf9fe965e1efb9ef
geotifulll-dev <geotifulll@gmail.com>
2026-04-23 18:08:48 +0400

    fix

--- scraper.js
+++ scraper.js
@@ -115,15 +115,14 @@ async function scrapeCarDetail(page, url) {
     console.log("🚀 სკრაპერი ჩაირთო...");
 
     const browser = await puppeteer.launch({
-        headless: "new",
-        args: [
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--disable-dev-shm-usage',
-            '--disable-gpu',
-            '--window-size=1920,1080'
-        ]
-    });
+    headless: "new",
+    args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-gpu'
+    ]
+});
 
     try {
         const mainPage = await browser.newPage();
